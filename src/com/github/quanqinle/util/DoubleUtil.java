64f30e148486b90a2c72e2f7f39d8edbc5132cd4
权芹乐 <qlquan@zhcpa.cn>
2018-01-25 14:48:42 +0800

    add funcs about double math

--- src/com/github/quanqinle/util/DoubleUtil.java
+++ src/com/github/quanqinle/util/DoubleUtil.java
@@ -1,26 +1,3 @@
-/*
- * Tencent is pleased to support the open source community by making
- * Tencent GT (Version 2.4 and subsequent versions) available.
- *
- * Notwithstanding anything to the contrary herein, any previous version
- * of Tencent GT shall not be subject to the license hereunder.
- * All right, title, and interest, including all intellectual property rights,
- * in and to the previous version of Tencent GT (including any and all copies thereof)
- * shall be owned and retained by Tencent and subject to the license under the
- * Tencent GT End User License Agreement (http://gt.qq.com/wp-content/EULA_EN.html).
- * 
- * Copyright (C) 2015 THL A29 Limited, a Tencent company. All rights reserved.
- * 
- * Licensed under the MIT License (the "License"); you may not use this file
- * except in compliance with the License. You may obtain a copy of the License at
- * 
- * http://opensource.org/licenses/MIT
- * 
- * Unless required by applicable law or agreed to in writing, software distributed
- * under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
- * CONDITIONS OF ANY KIND, either express or implied. See the License for the
- * specific language governing permissions and limitations under the License.
- */
 package com.github.quanqinle.util;
 
 import java.math.BigDecimal;
@@ -29,52 +6,119 @@ import java.math.BigDecimal;
  * Double数据的操作
  */
 public class DoubleUtil {
-    /**
-     * double 乘法
-     *
-     * @param d1
-     * @param d2
-     * @return
-     */
-    public static double mul(double d1, double d2) {
-        BigDecimal bd1 = new BigDecimal(Double.toString(d1));
-        BigDecimal bd2 = new BigDecimal(Double.toString(d2));
 
-        try {
-            return bd1.multiply(bd2).doubleValue();
-        } catch (Exception e) {
-            // 根据bugly观测，在进入GTOpMulPerfActivity页时有极小概率crash，故加上异常保护
-            // @see http://bugly.qq.com/detail?app=900010910&pid=1&ii=152#stack
-            e.printStackTrace();
-            return 0;
-        }
-    }
 
-    /**
-     * double 除法
-     *
-     * @param d1
-     * @param d2
-     * @param scale 四舍五入 小数点位数
-     * @return
-     */
-    public static double div(double d1, double d2, int scale) {
-        // 当然在此之前，你要判断分母是否为0，
-        // 为0你可以根据实际需求做相应的处理
+	// 默认除法运算精度
+	private static final int DEF_DIV_SCALE = 2;
+	// 默认乘法运算精度
+	private static final int DEF_MUL_SCALE = 2;
 
-        BigDecimal bd1 = new BigDecimal(Double.toString(d1));
-        BigDecimal bd2 = new BigDecimal(Double.toString(d2));
-        // return bd1.divide(bd2, scale, BigDecimal.ROUND_HALF_UP).doubleValue();
-        // 直接向下取整，保持和UI展示一致
-        try {
-            return bd1.divide(bd2, scale, BigDecimal.ROUND_DOWN).doubleValue();
-        } catch (Exception e) {
-            // 根据bugly观测，在进入GTOpMulPerfActivity页时有极小概率crash，故加上异常保护
-            // @see http://bugly.qq.com/detail?app=900010910&pid=1&ii=46#stack
-            e.printStackTrace();
-            return 0;
-        }
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+	}
 
-    }
+	// 这个类不能实例化
+	private DoubleUtil() {
+
+	}
+
+	/**
+	 * 提供精确的加法运算。
+	 * 
+	 * @param v1
+	 *            被加数
+	 * @param v2
+	 *            加数
+	 * @return 两个参数的和
+	 */
+	public static double add(double v1, double v2) {
+		BigDecimal b1 = new BigDecimal(Double.toString(v1));
+		BigDecimal b2 = new BigDecimal(Double.toString(v2));
+		return b1.add(b2).doubleValue();
+	}
+
+	/**
+	 * 提供精确的减法运算。
+	 * 
+	 * @param v1
+	 *            被减数
+	 * @param v2
+	 *            减数
+	 * @return 两个参数的差
+	 */
+	public static double sub(double v1, double v2) {
+		BigDecimal b1 = new BigDecimal(Double.toString(v1));
+		BigDecimal b2 = new BigDecimal(Double.toString(v2));
+		return b1.subtract(b2).doubleValue();
+	}
+
+	/**
+	 * 提供精确的乘法运算。
+	 * 
+	 * @param v1
+	 *            被乘数
+	 * @param v2
+	 *            乘数
+	 * @return 两个参数的积
+	 */
+	public static double mul(double v1, double v2) {
+		return mul(v1, v2, DEF_MUL_SCALE);
+	}
+
+	/**
+	 * 提供精确的乘法运算。
+	 * 
+	 * @param v1
+	 *            被乘数
+	 * @param v2
+	 *            乘数
+	 * @param scale
+	 *            需要精确到小数点以后几位
+	 * @return 两个参数的积
+	 */
+	public static double mul(double v1, double v2, int scale) {
+		BigDecimal b1 = new BigDecimal(Double.toString(v1));
+		BigDecimal b2 = new BigDecimal(Double.toString(v2));
+		return b1.multiply(b2).setScale(scale, BigDecimal.ROUND_HALF_UP).doubleValue();
+	}
+
+	/**
+	 * 提供（相对）精确的除法运算，当发生除不尽的情况时，精确到小数点以后2位，以后的数字四舍五入
+	 * 
+	 * @param v1
+	 *            被除数
+	 * @param v2
+	 *            除数
+	 * @return 两个参数的商
+	 */
+	public static double div(double v1, double v2) {
+		return div(v1, v2, DEF_DIV_SCALE);
+	}
+
+	/**
+	 * 提供（相对）精确的除法运算。当发生除不尽的情况时，由scale参数指 定精度，以后的数字“四舍五入”。
+	 * 
+	 * @param v1
+	 *            被除数
+	 * @param v2
+	 *            除数
+	 * @param scale
+	 *            需要精确到小数点以后几位
+	 * @return 两个参数的商
+	 */
+	public static double div(double v1, double v2, int scale) {
+		if (scale < 0) {
+			throw new IllegalArgumentException("The scale must be a positive integer or zero");
+		}
+		BigDecimal b1 = new BigDecimal(Double.toString(v1));
+		BigDecimal b2 = new BigDecimal(Double.toString(v2));
+		/**
+		 * ROUND_HALF_UP: 遇到.5的情况时往上近似,例: 1.5 ->2;
+		 * ROUND_HALF_DOWN : 遇到.5的情况时往下近似,例: 1.5->1;
+		 */
+		return b1.divide(b2, scale, BigDecimal.ROUND_HALF_UP).doubleValue();
+	}
 
 }
